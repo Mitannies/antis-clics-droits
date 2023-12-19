8dbe2f2f32fbb57bf15a9a1d1854224152eafc60
Mitannie <97797341+Mitannies@users.noreply.github.com>
2023-12-19 23:28:49 +0100

    Add files via upload

---
+++ cliquedroit.js
@@ -0,0 +1,8 @@
+// Fonction executée lors de l'utilisation du clic droit
+$(document).bind("contextmenu",function()
+{
+// Message d'alerte
+alert('Merci de respecter le travail du webmaster en ne copiant pas le contenu sans autorisation');
+return false;
+});
+// By WizzarCo.
